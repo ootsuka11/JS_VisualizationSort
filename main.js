25318e0af87a2473d93e665b39a5d59cc62555d1
Atsuya Ootsuka <ootsuka11@me.com>
2014-07-14 12:14:39 +0900

    make_bar

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-function make_bar(ID){
-	var date = {id : ID}
-	
-	$("#tmpl_bar").tmpl(date).appendTo("#bars");
+function make_bar(){
+	for(var i = 1; i <= 20; i++){
+ 		var date = {id : "#"+i+"bar",size : (i * 20) + "px", no: i}	
+ 		console.log(date);
+ 		$("#tmpl_bar").tmpl(date).appendTo("#bars");
+	}
+		
 	
 }
