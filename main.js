75b1bf599853959d0b050af5b311de1a60629828
Atsuya Ootsuka <ootsuka11@me.com>
2014-07-20 14:12:56 +0900

    random

--- main.js
+++ main.js
@@ -1,9 +1,46 @@
-function make_bar(){
-	for(var i = 1; i <= 20; i++){
- 		var date = {id : "#"+i+"bar",size : (i * 20) + "px", no: i}	
+// 管理用の配列
+var array = []
+
+// 指定した範囲のランダムな数を返す関数
+function random(min,max){
+	return Math.floor(Math.random()*((max+1)-min)+min);
+}
+
+// ランダムに配列を作成する(重複無し)
+function make_array(n){
+	for(var i = 1; i <= n; i++){
+		
+		// 空きが見つかるまで回り続ける
+		for(;;){
+			var a = random(0,n-1)
+				//console.log(a)
+			if(array[a] === undefined){
+				array[a] = i
+				break;
+			}
+		}
+		console.log(array)
+	}
+}
+
+//配列をもとにグラフを作成する
+function make_bar(n){
+	array = [];
+	make_array(n);
+	for(var i = 0; i < n; i++){
+ 		var date = {
+ 			id : "#"+array[i]+"bar",
+ 			size : (array[i] * n) + "px",
+ 			no: array[i]
+ 		}	
+		var pos = []
  		console.log(date);
  		$("#tmpl_bar").tmpl(date).appendTo("#bars");
 	}
 		
 	
 }
+
+$(function(){
+	$("#start").button().click(function( event ) {make_bar(20)});
+});
